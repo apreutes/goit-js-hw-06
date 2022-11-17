ed28a836b01c243773b339b4222fc02b30843f56
Sasha <aapreutes@gmail.com>
2022-11-17 17:25:26 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  button: document.querySelector(".change-color"),
+  span: document.querySelector(".color"),
+};
+
+const changeColor = (event) => {
+  const colorEl = getRandomHexColor(event);
+  refs.body.style.backgroundColor = colorEl;
+  refs.span.textContent = colorEl;
+};
+
+refs.button.addEventListener("click", changeColor);
